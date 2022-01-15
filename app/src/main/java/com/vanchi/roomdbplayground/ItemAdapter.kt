e6e19f7844bc05138fdb25b4e7f19de14f231cf6
vayipalayamchand <vayipalayamchand@paypal.com>
2022-01-15 11:22:46 -0600

    Adding Recycler View and Adapter to add records.

---
+++ app/src/main/java/com/vanchi/roomdbplayground/ItemAdapter.kt
@@ -0,0 +1,49 @@
+package com.vanchi.roomdbplayground
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.vanchi.roomdbplayground.databinding.ItemRowBinding
+
+class ItemAdapter(private val items:ArrayList<EmployeeEntity>): RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+
+    class ViewHolder(binding: ItemRowBinding): RecyclerView.ViewHolder(binding.root){
+        val llMain = binding.llMain
+        val tvName = binding.tvName
+        val tvEmail = binding.tvEmailAddress
+        val ivEdit = binding.ivEdit
+        val ivDelete = binding.ivDelete
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val context = holder.itemView.context
+        val item  = items[position]
+        holder.tvName.text = item.name
+        holder.tvEmail.text = item.email
+        if(position % 2 == 0){
+            holder.llMain.setBackgroundColor(ContextCompat.getColor(context,
+                R.color.lightGrey))
+        }else {
+            holder.llMain.setBackgroundColor(ContextCompat.getColor(context,
+                R.color.white))
+        }
+
+        holder.ivEdit.setOnClickListener{
+            //updateListener.invoke(item.id)
+        }
+
+        holder.ivDelete.setOnClickListener{
+            //deleteListener.invoke(item.id)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
--- app/src/main/java/com/vanchi/roomdbplayground/MainActivity.kt
+++ app/src/main/java/com/vanchi/roomdbplayground/MainActivity.kt
@@ -2,13 +2,19 @@ package com.vanchi.roomdbplayground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.vanchi.roomdbplayground.databinding.ActivityMainBinding
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,6 +23,12 @@ class MainActivity : AppCompatActivity() {
             addRecord(employeeDao)
         }
         setContentView(binding?.root)
+        lifecycleScope.launch{
+            employeeDao.loadAllEmployees().collect{ it ->
+                val list = ArrayList(it)
+                setUpDateInRecyclerView(list, employeeDao)
+            }
+        }
     }
 
     fun addRecord(employeeDao: EmployeeDao){
@@ -31,6 +43,19 @@ class MainActivity : AppCompatActivity() {
             }
         }else {
             Toast.makeText(applicationContext,"Please fill the fields", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun setUpDateInRecyclerView(employeeList: ArrayList<EmployeeEntity>, employeeDao: EmployeeDao){
+        if(employeeList.isNotEmpty()){
+            val itemAdapter = ItemAdapter(employeeList)
+            binding?.rvItemsList?.layoutManager = LinearLayoutManager(this)
+            binding?.rvItemsList?.adapter = itemAdapter
+            binding?.rvItemsList?.visibility = View.VISIBLE
+            binding?.tvNoRecordsAvailable?.visibility = View.GONE
+        }else{
+            binding?.rvItemsList?.visibility = View.GONE
+            binding?.tvNoRecordsAvailable?.visibility = View.VISIBLE
         }
     }
 }
