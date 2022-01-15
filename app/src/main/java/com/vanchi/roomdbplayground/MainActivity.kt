7ac5d5253baf6a06c9b7bfa6acc3a881b24ef35a
vanchinathan83 <ac.vanchinathan@gmail.com>
2022-01-15 14:05:02 -0600

    Adding the update and delete functionality
    Added update and delete listeners to the item adapter.

--- app/src/main/java/com/vanchi/roomdbplayground/MainActivity.kt
+++ app/src/main/java/com/vanchi/roomdbplayground/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.vanchi.roomdbplayground
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.vanchi.roomdbplayground.databinding.ActivityMainBinding
+import com.vanchi.roomdbplayground.databinding.DialogUpdateBinding
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
@@ -48,9 +51,17 @@ class MainActivity : AppCompatActivity() {
 
     fun setUpDateInRecyclerView(employeeList: ArrayList<EmployeeEntity>, employeeDao: EmployeeDao){
         if(employeeList.isNotEmpty()){
-            //val itemAdapter = ItemAdapter(employeeList)
+            val itemAdapter = ItemAdapter(employeeList,
+                {
+                    updateId ->
+                    updateRecordDialog(updateId, employeeDao)
+                },
+                {
+                    deleteId ->
+                    deleteRecordAlertDialog(deleteId, employeeDao)
+                })
             binding?.rvItemsList?.layoutManager = LinearLayoutManager(this)
-            //binding?.rvItemsList?.adapter = itemAdapter
+            binding?.rvItemsList?.adapter = itemAdapter
             binding?.rvItemsList?.visibility = View.VISIBLE
             binding?.tvNoRecordsAvailable?.visibility = View.GONE
         }else{
@@ -58,4 +69,79 @@ class MainActivity : AppCompatActivity() {
             binding?.tvNoRecordsAvailable?.visibility = View.VISIBLE
         }
     }
+
+    private fun updateRecordDialog(id:Int, employeeDao: EmployeeDao) {
+        val updateDialog = Dialog(this, R.style.Theme_Dialog)
+        updateDialog.setCancelable(false)
+        val dialogBinding = DialogUpdateBinding.inflate(layoutInflater)
+        updateDialog.setContentView(dialogBinding.root)
+
+        lifecycleScope.launch {
+            employeeDao.loadEmployeeById(id).collect {
+                if(it != null) {
+                    dialogBinding.etUpdateEmailId.setText(it.email)
+                    dialogBinding.etUpdateName.setText(it.name)
+                }
+            }
+        }
+
+        dialogBinding.tvUpdate.setOnClickListener {
+            lifecycleScope.launch {
+                val updatedName = dialogBinding.etUpdateName.text.toString()
+                val updatedEmail = dialogBinding.etUpdateEmailId.text.toString()
+                if (updatedName.isNotEmpty() && updatedEmail.isNotEmpty()) {
+                    employeeDao.update(EmployeeEntity(id, updatedName, updatedEmail))
+                    Toast.makeText(
+                        applicationContext,
+                        "Updated Record Successfully",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    updateDialog.dismiss()
+                } else {
+                    Toast.makeText(
+                        applicationContext,
+                        "Name and Email are required for update!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+        dialogBinding.tvCancel.setOnClickListener{
+            updateDialog.dismiss()
+        }
+        updateDialog.show()
+    }
+
+    private fun deleteRecordAlertDialog(id:Int, employeeDao: EmployeeDao){
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("Delete Record")
+        alertDialogBuilder.setIcon(android.R.drawable.ic_dialog_alert)
+
+        lifecycleScope.launch {
+            employeeDao.loadEmployeeById(id).collect {
+                if(it != null){
+                    alertDialogBuilder.setMessage("Do you want to delete ${it.name}")
+                }
+            }
+        }
+
+        alertDialogBuilder.setPositiveButton("Yes"){ dialogInterface, _ ->
+            lifecycleScope.launch {
+                employeeDao.delete(EmployeeEntity(id))
+                Toast.makeText(applicationContext,
+                    "Deleted Record Successfully!", Toast.LENGTH_SHORT).show()
+            }
+            dialogInterface.dismiss()
+        }
+
+        alertDialogBuilder.setNegativeButton("No"){dialogInterface, _ ->
+            dialogInterface.dismiss()
+        }
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.setCancelable(false)
+        alertDialog.show()
+    }
+
+
 }
